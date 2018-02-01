3aad7c4d5b56a9d05ba3c6e5c231c819203cb269
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-01-31 18:15:48 -0600

    wrote our first capybara system test

---
+++ spec/system/add_project_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "adding a project", type: :system do
+  it "allows a user to create a project with tasks" do
+    visit new_project_path
+    fill_in "Name", with: "Project Runway"
+    fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5"
+    click_on "Create Project"
+    visit projects_path
+    expect(page).to have_content("Project Runway")
+    expect(page).to have_content(8)
+  end
+end
