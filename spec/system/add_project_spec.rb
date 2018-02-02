173cf54110978355fd56f9c3befedfe1890e4038
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-02-02 08:27:45 -0600

    capybara sytem testing...

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "adding a project", type: :system do
-  it "allows a user to create a project with tasks" do
+  it "allows a user to create a project with tasks", :pending do
     visit new_project_path
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5"
