7d644a9a83ad6ac9199548199235806036b49ac1
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-02-12 15:18:42 -0600

    Projects#create create, invoke, and redirect

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,12 @@ class ProjectsController < ApplicationController
   def new
     @project = Project.new
   end
-  def create
 
+  def create
+    @workflow = CreatesProject.new(
+      name: params[:project][:name],
+      task_string: params[:project][:tasks])
+    @workflow.create
+    redirect_to projects_path
   end
 end
