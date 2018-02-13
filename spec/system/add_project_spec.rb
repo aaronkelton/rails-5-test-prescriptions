3d4330b22665dad859275373398c963e2f08dd07
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-02-13 08:14:06 -0600

    I think I'm on page 62, dunno DART things

--- spec/system/add_project_spec.rb
+++ spec/system/add_project_spec.rb
@@ -1,13 +1,18 @@
 require "rails_helper"
 
 RSpec.describe "adding a project", type: :system do
+
   it "allows a user to create a project with tasks" do
     visit new_project_path
     fill_in "Name", with: "Project Runway"
     fill_in "Tasks", with: "Choose Fabric:3\nMake it Work:5"
     click_on "Create Project"
     visit projects_path
-    expect(page).to have_content("Project Runway")
-    expect(page).to have_content(8)
+    @project = Project.find_by(name: "Project Runway")
+    expect(page).to have_selector(
+      "#project_#{@project.id} .name", text: "Project Runway")
+    expect(page).to have_selector(
+      "#project_#{@project.id} .total-size", text: "8")
   end
+
 end
