c761260c1846e8cdf7d4f51b0fa5a4680f601ea4
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-08 08:50:27 -0500

    added description and failure messages to custom matcher

--- spec/support/size_matcher.rb
+++ spec/support/size_matcher.rb
@@ -2,4 +2,16 @@ RSpec::Matchers.define :be_of_size do |expected|
   match do |actual|
     actual.total_size == expected
   end
+
+  description do
+    "have tasks totaling #{expected} points"
+  end
+
+  failure_message do |actual|
+    "expected project #{actual.name} to have size #{expected}, was #{actual}"
+  end
+
+  failure_message_when_negated do |actual|
+    "expected project #{actual.name} not to have size #{expected}, but it did"
+  end
 end
