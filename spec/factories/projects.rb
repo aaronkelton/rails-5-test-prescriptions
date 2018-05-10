c44f9282aa6469e1e36a995d7f52a1cc7db740bb
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-10 09:33:07 -0500

    moved traits per book but same lint notification

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -3,4 +3,6 @@ FactoryBot.define do
     name "Project Runway"
     due_date 1.week.from_now
   end
+  trait :soon do due_date { 1.day.from_now } end
+  trait :later do due_date { 1.month.from_now } end
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,12 +3,11 @@ FactoryBot.define do
     sequence(:title) { |n| "Task #{n}" }
     size 3
     completed_at nil
+    project
   end
 
   trait :small do size 1 end
   trait :large do size 5 end
-  trait :soon do due_date { 1.day.from_now } end
-  trait :later do due_date { 1.month.from_now } end
   trait :newly_complete do completed_at { 1.day.ago } end
   trait :long_complete do completed_at { 6.months.ago } end
 
