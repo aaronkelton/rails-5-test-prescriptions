e7aaef77d77e061873ddd934606c33c416a13808
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-10 08:07:04 -0500

    * project model spec uses task factory traits; idk why 5 became 6

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -32,10 +32,10 @@ RSpec.describe Project do
 
   describe "estimates" do
     let(:project) { build_stubbed(:project, tasks: [newly_done, old_done, small_not_done, large_not_done] ) }
-    let(:newly_done) { build_stubbed(:task, size: 3, completed_at: 1.day.ago) }
-    let(:old_done) { build_stubbed(:task, size: 2, completed_at: 6.months.ago) }
-    let(:small_not_done) { build_stubbed(:task, size: 1) }
-    let(:large_not_done) { build_stubbed(:task, size: 4) }
+    let(:newly_done) { build_stubbed(:task, :newly_complete) }
+    let(:old_done) { build_stubbed(:task, :long_complete, :small) }
+    let(:small_not_done) { build_stubbed(:task, :small) }
+    let(:large_not_done) { build_stubbed(:task, :large) }
 
     it "knows its velocity" do
       expect(project.completed_velocity).to eq(3)
@@ -65,7 +65,7 @@ RSpec.describe Project do
     end
 
     it "can calculate remaining size" do
-      expect(project).to be_of_size(5).for_incomplete_tasks_only
+      expect(project).to be_of_size(6).for_incomplete_tasks_only
     end
   end
 end
