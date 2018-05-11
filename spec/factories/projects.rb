211e3b4559b1f933cd9bc42b785121b31e9d41a9
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-11 08:10:42 -0500

    use relative dates

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :project do
     name "Project Runway"
     due_date 1.week.from_now
+    start_date 1.week.ago
   end
   trait :soon do due_date { 1.day.from_now } end
   trait :later do due_date { 1.month.from_now } end
--- spec/unit/project_spec.rb
+++ spec/unit/project_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "some Rappin unit test" do
   describe "without a task" do
-    let(:runway) { create(:project, name: "Project Runway", start_date: "2018-01-20") }
-    let(:greenlight) { create(:project, name: "Project Greenlight", start_date: "2018-02-24") }
-    let(:gutenberg) { create(:project, name: "Project Gutenberg", start_date: "2018-01-31") }
+    let(:runway) { create(:project, name: "Project Runway", start_date: 1.week.ago) }
+    let(:greenlight) { create(:project, name: "Project Greenlight", start_date: 1.month.ago) }
+    let(:gutenberg) { create(:project, name: "Project Gutenberg", start_date: 1.day.ago) }
 
     it "finds recently started projects" do
       actual = Project.find_recently_started(6.months)
