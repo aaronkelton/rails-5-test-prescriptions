6b1a18b8beadce98b217d1e72da19e419cd35600
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-05-23 08:23:14 -0500

    add capybara screenshot gem and configure

--- Gemfile
+++ Gemfile
@@ -53,6 +53,7 @@ group :development do
 end
 
 group :test do
+  gem 'capybara-screenshot'
   gem 'database_cleaner'
 end
 
--- spec/support/system.rb
+++ spec/support/system.rb
@@ -1,5 +1,7 @@
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    driven_by :rack_test
+    driven_by :rack_test # Capybara's internal Ruby driver sans RSpec
   end
 end
+
+require "capybara-screenshot/rspec"
